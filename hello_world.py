830fa652ec1a004b564228e932447d10c61f51a1
Malakias <malakias.kosonen@tuni.fi>
2022-04-13 19:16:57 +0300

    Hello World! V2.1

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+"""
+Git Open
+Malakias Kosonen, malakias.kosonen@tuni.fi
+Opiskelijanumero: 050238327
+Tehtävä 2.8:
+"""
+
+
+def main():
+
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
